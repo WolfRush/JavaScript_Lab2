3525a51c692451799a1562bc66b5c96f52e58209
WolfRush <johan.a.mossberg@gmail.com>
2025-12-22 15:37:49 +0100

    Added IDs to buttons and referenced form fields in JS
    Assigned unique IDs to the buttons in contact.html. Updated contact.js to reference form fields and buttons by their IDs and add them to varibles.

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,1 +1,12 @@
 console.log("Contact page script loaded.");
+
+let firstName = document.getElementById('first-name');
+let lastName = document.getElementById('last-name');
+let email = document.getElementById('email');
+let phone = document.getElementById('phone');
+let subject = document.getElementById('subject');
+let message = document.getElementById('message');
+let sendButton = document.getElementById('form-send');
+let clearButton = document.getElementById('form-clear');
+
+console.log(firstName, lastName, email, phone, subject, message, sendButton, clearButton);
