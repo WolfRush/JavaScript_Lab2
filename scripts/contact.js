4b58fffc8d55a079ba815bc1adb2d34bec563d9e
WolfRush <johan.a.mossberg@gmail.com>
2025-12-29 15:46:05 +0100

    Improve contact form validation and error display
    <span> elements for error messages in contact.html and assigned unique IDs for each error field. Updated contact.js to display specific error messages for invalid first and last names, and prevent form submission until errors are corrected.

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,5 +1,6 @@
 console.log("Contact page script loaded.");
 
+// Form field elements
 let firstName = document.getElementById('first-name');
 let lastName = document.getElementById('last-name');
 let email = document.getElementById('email');
@@ -9,8 +10,18 @@ let message = document.getElementById('message');
 let sendButton = document.getElementById('form-send');
 let clearButton = document.getElementById('form-clear');
 
-// Validation that name is only letters
+//Error message elements 
+let firstNameError = document.getElementById('first-name-error');
+let lastNameError = document.getElementById('last-name-error');
+let emailError = document.getElementById('email-error');
+let phoneError = document.getElementById('phone-error');
+let subjectError = document.getElementById('subject-error');
+let messageError = document.getElementById('message-error');
 
+
+let isValid = true;
+
+// Validation that name is only letters
 function validateName(name) {
     let nameRegex = /^[A-Za-z]+$/;
     return nameRegex.test(name);
@@ -18,9 +29,20 @@ function validateName(name) {
 
 sendButton.addEventListener('click', function(event) {
     event.preventDefault(); // Prevent form submission for validation
-    if (!validateName(firstName.value) || !validateName(lastName.value)) {
-        alert("Please enter valid names using only letters.");
-        return;
+    if (!validateName(firstName.value)) {
+        firstNameError.textContent = "First name must contain only letters.";
+        isValid = false;
     }
-    alert("Form submitted successfully!" + "\nFirst Name: " + firstName.value + "\nLast Name: " + lastName.value + "\nEmail: " + email.value + "\nPhone: " + phone.value + "\nSubject: " + subject.value +  "\nMessage: " + message.value);
+    if (!validateName(lastName.value)) {
+        lastNameError.textContent = "Last name must contain only letters.";
+        isValid = false;
+    }
+
+    if (isValid) {
+        alert("Form submitted successfully!" + "\nFirst Name: " + firstName.value + "\nLast Name: " + lastName.value + "\nEmail: " + email.value + "\nPhone: " + phone.value + "\nSubject: " + subject.value +  "\nMessage: " + message.value);
+    }
+    else {
+        alert("Please correct the errors in the form before submitting.");
+    }
+    
 });
