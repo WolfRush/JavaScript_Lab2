44f9608b8b8142e25f66537cfedcc6910f521345
WolfRush <johan.a.mossberg@gmail.com>
2026-01-03 16:06:26 +0100

    Created projects CSS & script files and added data array
    Added projects.js & projects.css. Updated projects.html to load the new script and stylesheet, and commented the static project markup.

---
+++ scripts/projects.js
@@ -0,0 +1,59 @@
+console.log("Projects page script loaded.");
+
+const projects = [
+{
+id: 1,
+title: "Blast from the Past",
+description: "An non violent movement shooter game built for time trails and exploration. This is my biggest solo project yet.",
+category: "Game",
+technologies: ["Unity", "C#"],
+image: "assets/blastfromthepast_pro.jpg",
+link: "https://example.com"
+},
+{
+id: 2,
+title: "Bug-Bug",
+description: "A quirky multiplayer game featuring unique coop ball rolling mechanics. I was one of the lead game designers and was responsible of VFX in this 12-person project.",
+category: "Game",
+technologies: ["Unity", "C#"],
+image: "assets/bugbug_pro.png",
+link: "https://alltimefelix.itch.io/bugbug-a-po-op-game"
+},
+{
+id: 3,
+title: "Arm the farm",
+description: "A tower defense game with farming elements. Responsible for core gameplay programming and overall game design direction.",
+category: "Game",
+technologies: ["Unity", "C#"],
+image: "assets/armthefarm_pro.png",
+link: "https://example.com"
+},
+{
+id: 4,
+title: "Deadlock Modding",
+description: "I am currenty working on some mods to the game Deadlock. It's one of the reasons i am trying to learn Blender but at the same time I am also learning about the Source 2 engine.",
+category: "Modding",
+technologies: ["Blender", "Source 2"],
+image: "assets/grayertalonmod_pro.jpg",
+link: "https://gamebanana.com/mods/629048"
+},
+{
+id: 5,
+title: "Portfolio Website",
+description: "This very website you are on right now! Built to showcase my projects and skills as a developer and designer.",
+category: "Web Development",
+technologies: ["HTML", "CSS", "JavaScript"],
+image: "assets/portfolio_website_pro.png",
+link: "index.html"
+},
+{
+id: 6,
+title: "Workout Manager Website",
+description: "A simple task management application to help users organize their daily workout activities. Features include task creation, editing, and deletion.",
+category: "Web Development",
+technologies: ["HTML", "CSS", "JavaScript"],
+image: "assets/task_manager_app_pro.png",
+link: "http://mossymound.site/johmos-5/uppgift4/html/index.html#main-content"
+}
+];
+
