2eb09a34a595e2371bf6f7f200e7c7be503cdb77
WolfRush <johan.a.mossberg@gmail.com>
2026-01-03 21:30:01 +0100

    Revert "Add fade-in animation to project cards"
    This reverts commit c40f0cdf92b039ea7d133e8b205284939a0a5d12.

--- scripts/projects.js
+++ scripts/projects.js
@@ -73,9 +73,8 @@ function displayProjects(filteredArray = projects) {
         filterCounter.textContent = `Showing ${filteredArray.length} of ${projects.length} projects`;
     }
 
-    // Show only first 2 projects on index page
     if (currentPage === 'index.html') {
-        filteredArray = filteredArray.slice(0, 2); 
+        filteredArray = filteredArray.slice(0, 2); // Show only first 2 projects on index page
     }
 
     // Loop through all projects and create cards
@@ -120,15 +119,7 @@ function displayProjects(filteredArray = projects) {
         container.addEventListener('click', () => {
             window.open(project.link, '_blank');
         });
-
-        // Card fade-in effect
-        container.classList.add('fade-in');
         
-        // Remove fade-in class after animation completes to allow hover
-        container.addEventListener('animationend', () => {
-            container.classList.remove('fade-in');
-        }, { once: true });
-
         // Add to project list
         projectList.appendChild(container);
     });
